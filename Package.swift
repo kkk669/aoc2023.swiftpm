bac522ec2521482f39e57aedbe1fe17c494c7fba
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2023-12-02 01:27:57 +0900

    style: change swift-format rules

--- Package.swift
+++ Package.swift
@@ -13,25 +13,30 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-algorithms.git",
-            .upToNextMajor(from: "1.2.0")),
+            .upToNextMajor(from: "1.2.0")
+        ),
         .package(
             url: "https://github.com/apple/swift-collections.git",
-            .upToNextMajor(from: "1.0.0")),
+            .upToNextMajor(from: "1.0.0")
+        ),
         .package(
             url: "https://github.com/apple/swift-argument-parser.git",
-            .upToNextMajor(from: "1.2.0")),
+            .upToNextMajor(from: "1.2.0")
+        ),
         .package(
             url: "https://github.com/apple/swift-format.git",
-            .upToNextMajor(from: "509.0.0"))
+            .upToNextMajor(from: "509.0.0")
+        ),
     ],
     targets: [
         .executableTarget(
             name: "AdventOfCode",
             dependencies: dependencies,
-            resources: [.copy("Data")]),
+            resources: [.copy("Data")]
+        ),
         .testTarget(
             name: "AdventOfCodeTests",
             dependencies: ["AdventOfCode"] + dependencies
-        )
+        ),
     ]
 )
